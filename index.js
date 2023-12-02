ed9132377f60550030d826dec2ee035dbce69f35
nisu.ahmad <nisu.ahmad@samarth.ac.in>
2023-12-02 11:29:58 +0530

    add environmnet variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const app= express();
-const PORT=2000;
+require("dotenv").config();
+const PORT=process.env.PORT||2000;
 
 app.get('/',(req,res)=>{
     res.send("Hello");
@@ -10,5 +11,5 @@ app.listen(PORT,(err)=>{
     if(err){
         console.log("Some Error occured");
     }
-    console.log(`Successfully running on port http//:localhost:${PORT}`)
+    console.log(`Successfully running on port http://localhost:${PORT}`)
 })
